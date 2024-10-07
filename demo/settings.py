e5f73445f14ea3e298fc3c2cdd0e379b9f94a7e1
William Rodrigues <williamr723@gmail.com>
2024-10-06 23:11:16 -0300

    "Added import of constants from django.contrib.messages and defined MESSAGE_TAGS in settings.py"

--- demo/settings.py
+++ demo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -127,5 +128,14 @@ if not DEBUG:    # Tell Django to copy static assets into a path called `staticf
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+    constants.WARNING: 'alert-warning',
+}
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
